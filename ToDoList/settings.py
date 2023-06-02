a0efa029dad906756c5e77a9556207e0db3642bd
AKASH A S <110691415+akashasgowda@users.noreply.github.com>
2023-06-02 22:33:58 +0530

    Update settings.py
    Allowing pythonanywhere host

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u(#9(n3iqtb5t46(kx_7rm4g&44=w&nwe5=1ng3ag@x$gpb@f3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akashasgowda.pythonanywhere.com']
 
 
 # Application definition
